9908fd7a6d887f34e6adfc9fdc8341b1d303b8be
rodrfct <rodrigoca.fct@gmail.com>
2023-05-22 18:26:42 +0200

    Balance component finished Plus some project setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script setup>
+import Balance from './components/Balance.vue';
+import Spending from './components/Spending.vue';
+
 
 </script>
 
 <template>
+  <main>
+    <Balance />
 
+    <Spending />
+  </main>
 </template>
 
 <style scoped>
+  main {
+    width: 300px;
 
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  main > * {
+    margin: 5% 0;
+  }
 </style>
---
+++ src/components/Balance.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card">
+        <div class="text">
+            <p>My balance</p>
+            <p class="balance">$921.48</p>
+        </div>
+
+        <img src="../assets/img/logo.svg" alt="">
+    </div>
+</template>
+
+<style scoped>
+.card {
+    background-color: var(--soft-red);
+    border-radius: 15px;
+    padding: 1rem 1.5rem;
+    color: var(--very-pale-orange);
+
+    display: flex;
+    justify-content: space-between;
+}
+
+p {
+    font-size: .7rem;
+    margin: .5rem 0;
+}
+
+.balance {
+    font-size: 1.2rem;
+    font-weight: 700;
+}
+</style>
